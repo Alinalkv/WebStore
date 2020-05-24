50231cc43fbe658bc98089aa0d245c9362ede0ca
Алина Лыкова <alilykka@gmail.com>
2020-05-24 18:30:53 +0300

    Модель Employee

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -3,14 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> _Employees = new List<Employee> {
+
+            new Employee
+            {
+                Id = 1,
+                FirstName = "Иван",
+                SecondName = "Иванович",
+                Surname = "Иванов",
+                Age = 28
+            },
+            new Employee
+            {
+                Id = 2,
+                FirstName = "Пётр",
+                SecondName = "Петрович",
+                Surname = "Петров",
+                Age = 29
+            },
+            new Employee
+            {
+                Id = 3,
+                FirstName = "Алесандр",
+                SecondName = "Алесандрович",
+                Surname = "Алесандров",
+                Age = 28
+            },
+            };
+
+
+
         public IActionResult Index()
         {
-            return View();
+            ViewData["Title"] = "Представление Home.Index";
+            return View(_Employees);
         }
 
         public IActionResult AnotherAction()
