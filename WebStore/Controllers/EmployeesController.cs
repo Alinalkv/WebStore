67db02f253756b0a9a27453b745dc7a559122013
Алина Лыкова <alilykka@gmail.com>
2020-05-30 10:15:30 +0300

    Добавила класс TestData для хранения сотрудников

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Data;
 using WebStore.Models;
 
 namespace WebStore.Controllers
@@ -10,38 +11,11 @@ namespace WebStore.Controllers
     public class EmployeesController : Controller
     {
 
-        private static readonly List<Employee> _Employees = new List<Employee> {
-
-            new Employee
-            {
-                Id = 1,
-                FirstName = "Иван",
-                SecondName = "Иванович",
-                Surname = "Иванов",
-                Age = 28
-            },
-            new Employee
-            {
-                Id = 2,
-                FirstName = "Пётр",
-                SecondName = "Петрович",
-                Surname = "Петров",
-                Age = 29
-            },
-            new Employee
-            {
-                Id = 3,
-                FirstName = "Алесандр",
-                SecondName = "Алесандрович",
-                Surname = "Алесандров",
-                Age = 28
-            },
-            };
+        private static readonly List<Employee> _Employees = TestData.Employees;
         public IActionResult Index()
         {
             return View(_Employees);
         }
-
 
         public IActionResult EmployeeDetails(int id)
         {
