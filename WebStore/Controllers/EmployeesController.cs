04ac427c3c97ff069f20691be3861f104b17df83
Алина Лыкова <alilykka@gmail.com>
2020-05-30 11:30:59 +0300

    Добавлен интерфейс в контроллер

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebStore.Data;
+using WebStore.Infrustructure.Interfaces;
 using WebStore.Models;
 
 namespace WebStore.Controllers
@@ -11,15 +12,21 @@ namespace WebStore.Controllers
     public class EmployeesController : Controller
     {
 
-        private static readonly List<Employee> _Employees = TestData.Employees;
+        private readonly IEmployeesData _EmployeesData;
+
+        public EmployeesController(IEmployeesData EmployeesData)
+        {
+            _EmployeesData = EmployeesData;
+        }
+
         public IActionResult Index()
         {
-            return View(_Employees);
+            return View(_EmployeesData.Get());
         }
 
         public IActionResult EmployeeDetails(int id)
         {
-            var employee = _Employees.FirstOrDefault(c => c.Id == id);
+            var employee = _EmployeesData.GetById(id);
             if (employee is null)
                 return NotFound();
             return View(employee);
