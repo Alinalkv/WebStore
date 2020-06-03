cd2cff90f5fd0b7301b817afffdd4bd5e8ca9946
Алина Лыкова <alilykka@gmail.com>
2020-06-03 19:58:08 +0300

    Секции во вью компонент

---
+++ WebStore/Components/SectionsViewComponent.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Infrustructure.Interfaces;
+using WebStore.ViewModels;
+
+namespace WebStore.Components
+{
+    public class SectionsViewComponent : ViewComponent
+    {
+       private readonly IProductData _ProductData;
+        public SectionsViewComponent (IProductData ProductData)
+        {
+            _ProductData = ProductData;
+        }
+        
+        //во вью компоненте дб invoke
+        //делаем какую-то логику и возвращаем представление
+        public IViewComponentResult Invoke() => View(GetSections());
+
+        private IEnumerable<SectionViewModel> GetSections()
+        {
+            var sections = _ProductData.GetSections();
+            var parent_sections = sections.Where(c => c.ParentId is null);
+            var parent_sections_views = parent_sections.Select(s => new SectionViewModel
+            {
+                Id = s.Id,
+                Order = s.Order,
+                Name = s.Name
+            }).ToList();
+
+            foreach (var parent_section in parent_sections_views)
+            {
+                var child_sections = sections.Where(c => c.ParentId == parent_section.Id);
+                foreach (var child in child_sections)
+                {
+                    parent_section.ChildSections.Add(new SectionViewModel {
+                        Id = child.Id,
+                        Order = child.Order,
+                        Name = child.Name,
+                        ParentSection = parent_section
+                    });
+                }
+                parent_section.ChildSections.Sort((a, b) => Comparer<double>.Default.Compare(a.Order, b.Order));
+            }
+
+            parent_sections_views.Sort((a, b) => Comparer<double>.Default.Compare(a.Order, b.Order));
+
+            return parent_sections_views;
+        }
+    }
+}
--- WebStore/ViewModels/SectionViewModel.cs
+++ WebStore/ViewModels/SectionViewModel.cs
@@ -11,9 +11,7 @@ namespace WebStore.ViewModels
         public int Id { get; set; }
         public string Name { get; set; }
         public int Order { get; set; }
-
         public List<SectionViewModel> ChildSections { get; set; } = new List<SectionViewModel>();
-
-        public SectionViewModel ParentSection { get; set }
+        public SectionViewModel ParentSection { get; set; }
     }
 }
