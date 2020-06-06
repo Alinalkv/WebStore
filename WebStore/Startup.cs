6ddad8ec6d242a57520c6a5b1de97e14b1cc7810
Алина Лыкова <alilykka@gmail.com>
2020-06-06 13:33:29 +0300

    Подключени бд через контейнер сервисов

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebStore.DAL.Context;
 using WebStore.Infrustructure.Interfaces;
 using WebStore.Infrustructure.Services;
 
@@ -21,6 +23,7 @@ namespace WebStore
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<WebStoreDB>(opt => opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
             //зарегали сервис для работы с сотруниками
             services.AddSingleton<IEmployeesData, InMemoryEmployeesData>();
