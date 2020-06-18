655ccf23eff33785803ba6a9d83e65abc59ea456
Алина Лыкова <alilykka@gmail.com>
2020-06-18 19:56:49 +0300

    Интерфейс Корзины

---
+++ WebStore/Infrustructure/Interfaces/ICartService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.ViewModels;
+
+namespace WebStore.Infrustructure.Interfaces
+{
+    public interface ICartService
+    {
+        void AddToCart(int id);
+
+        void DecrementFromCart(int id);
+
+        void RemoveFromCart(int id);
+
+        void RemoveAll();
+
+        CartViewModel TransformFromCart();
+
+    }
+}
