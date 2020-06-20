55203144b0f2798af6eb704e3d4fe1587779cb2d
Алина Лыкова <alilykka@gmail.com>
2020-06-20 17:36:21 +0300

    Добавлен сервис для работы с заказами

--- WebStore/Infrustructure/Services/InSQL/SqlOrderService.cs
+++ WebStore/Infrustructure/Services/InSQL/SqlOrderService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,19 +22,54 @@ namespace WebStore.Infrustructure.Services.InSQL
             _db = db;
             _UserManager = UserManager;
         }
-        public Task<Order> CreateOrder(string UserName, CartViewModel Cart, OrderViewModel OrderModel)
+        public async Task<Order> CreateOrder(string UserName, CartViewModel Cart, OrderViewModel OrderModel)
         {
-            throw new NotImplementedException();
+            var user = await _UserManager.FindByNameAsync(UserName);
+            if (user is null)
+                throw new InvalidOperationException($"Пользователь {UserName} не найден");
+
+            await using var transaction = await _db.Database.BeginTransactionAsync();
+            var order = new Order
+            {
+                Address = OrderModel.Address,
+                User = user,
+                Date = DateTime.Now,
+                Name = OrderModel.Name,
+                Phone = OrderModel.Phone,
+                Items = new List<OrderItem>()
+            };
+
+            foreach (var (product_model, quantity) in Cart.Items)
+            {
+                var product = await _db.Products.FindAsync(product_model.Id);
+                if(product is null)
+                    throw new InvalidOperationException($"Товар Id:{product_model.Id} не найден");
+                var order_item = new OrderItem
+                {
+                    Order = order,
+                    Price = product.Price,
+                    Product = product,
+                    Quantity = quantity
+                };
+
+                order.Items.Add(order_item);
+            }
+            await _db.Orders.AddAsync(order);
+            await _db.SaveChangesAsync();
+
+            await transaction.CommitAsync();
+
+            return order;
         }
 
-        public Task<Order> GetOrderById(int id)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task<Order> GetOrderById(int id) => await _db.Orders
+            .Include(o => o.Items)
+            .FirstOrDefaultAsync(o => o.Id == id);
 
-        public Task<IEnumerable<Order>> GetUserOrders(string UserName)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task<IEnumerable<Order>> GetUserOrders(string UserName) => await _db.Orders
+            .Include(o => o.User)
+            .Include(o => o.Items)
+            .Where(o => o.User.UserName == UserName)
+            .ToArrayAsync();
     }
 }
