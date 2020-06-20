ed81cffdcc76d792507e443f6d483cbf5e8e7ded
Алина Лыкова <alilykka@gmail.com>
2020-06-20 13:59:42 +0300

    Сделана Вью Index

--- WebStore/Areas/Admin/Controllers/CatalogController.cs
+++ WebStore/Areas/Admin/Controllers/CatalogController.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Infrustructure.Interfaces;
 
 namespace WebStore.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CatalogController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        private readonly IProductData _ProductData;
+
+        public CatalogController(IProductData ProductData) =>  _ProductData = ProductData;
+        public IActionResult Index() => View(_ProductData.GetProducts());
     }
 }
