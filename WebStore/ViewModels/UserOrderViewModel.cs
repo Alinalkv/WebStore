189fa054a9fb3d0c35d09d2c6a4baa5a2d9ef8d3
Алина Лыкова <alilykka@gmail.com>
2020-06-20 19:15:16 +0300

    Проифль юзера

---
+++ WebStore/ViewModels/UserOrderViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.ViewModels
+{
+    public class UserOrderViewModel
+    {
+        //id пользователя
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        //сумма заказа
+        public decimal TotalSum { get; set; }
+    }
+}
