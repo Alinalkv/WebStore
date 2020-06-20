b38b420744e18625a3152cf7432316826eacf9d6
Алина Лыкова <alilykka@gmail.com>
2020-06-20 16:13:47 +0300

    Заготовка сервиса заказов

---
+++ WebStore/Infrustructure/Services/InSQL/SqlOrderService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Infrustructure.Interfaces;
+
+namespace WebStore.Infrustructure.Services.InSQL
+{
+    public class SqlOrderService : IOrderService
+    {
+    }
+}
