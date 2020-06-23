17c949cd657df6d06b1cef1e5736c7cc07c2280f
Алина Лыкова <alilykka@gmail.com>
2020-06-23 22:35:01 +0300

    Половина контроллера

---
+++ Services/WebStore.ServiceHosting/Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private static readonly List<string> _Values = Enumerable
+            .Range(1, 10)
+            .Select(i => $"Value - {i}")
+            .ToList();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get() => _Values;
+
+        public ActionResult<string> Get(int id)
+        {
+            if (id < 0)
+                return BadRequest();
+            if (id >= _Values.Count)
+                return NotFound();
+            return _Values[id];
+        }
+    }
+}
