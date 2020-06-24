e3101408b6fda054ef0994a580181730cd77f196
Алина Лыкова <alilykka@gmail.com>
2020-06-24 19:19:39 +0300

    Контроллер для TestAPI

--- Services/WebStore.ServiceHosting/Controllers/ValuesController.cs
+++ Services/WebStore.ServiceHosting/Controllers/ValuesController.cs
@@ -19,6 +19,7 @@ namespace WebStore.ServiceHosting.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get() => _Values;
 
+        [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
             if (id < 0)
@@ -26,6 +27,26 @@ namespace WebStore.ServiceHosting.Controllers
             if (id >= _Values.Count)
                 return NotFound();
             return _Values[id];
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, string value)
+        {
+            if (id < 0 || id >= _Values.Count)
+                return BadRequest();
+            _Values[id] = value;
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            if (id < 0)
+                return BadRequest();
+            if (id >= _Values.Count)
+                return NotFound();
+            _Values.RemoveAt(id);
+            return Ok();
         }
     }
 }
