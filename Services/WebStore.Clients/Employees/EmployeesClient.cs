ea1ff018ed42206cc4bae8e98deb6ff607a856d3
Алина Лыкова <alilykka@gmail.com>
2020-06-26 21:06:45 +0300

    Заготовка клиента Employee

---
+++ Services/WebStore.Clients/Employees/EmployeesClient.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Clients.Base;
+using WebStore.Domain;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Clients.Employees
+{
+    public class EmployeesClient : BaseClient, IEmployeesData
+    {
+        public EmployeesClient(IConfiguration Configuration) : base(Configuration, WebApi.Employees) { }
+
+        public int Add(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
