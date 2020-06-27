c26d5ff82df51734bc4020bc8e4b3e1617ca3181
Алина Лыкова <alilykka@gmail.com>
2020-06-27 14:31:55 +0300

    Сделан мэппер

---
+++ Services/WebStore.Services/Mapping/OrderMapper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebStore.Domain.DTO.Order;
+using WebStore.Domain.Entities.Orders;
+
+namespace WebStore.Services.Mapping
+{
+    public static class OrderMapper
+    {
+        public static OrderDTO ToDTO(this Order o) => o is null ? null : new OrderDTO
+        {
+             Address = o.Address,
+             Date = o.Date,
+             Id = o.Id,
+             Items = o.Items.Select(ToDTO),
+             Name = o.Name,
+             Phone = o.Phone,
+        };
+        public static Order FromDTO(this OrderDTO o) => o is null ? null : new Order
+        {
+             Address = o.Address,
+             Phone = o.Phone,
+             Date = o.Date,
+             Id = o.Id,
+             Items = o.Items.Select(FromDTO).ToArray(),
+             Name = o.Name,
+              
+        };
+
+        public static OrderItemDTO ToDTO(this OrderItem o) => o is null ? null : new OrderItemDTO
+        {
+             Id = o.Id,
+              Price = o.Price,
+               Quantity = o.Quantity,
+        };
+
+        public static OrderItem FromDTO(this OrderItemDTO o) => o is null ? null : new OrderItem
+        {
+            Id = o.Id,
+            Price = o.Price,
+            Quantity = o.Quantity,
+        };
+    }
+}
