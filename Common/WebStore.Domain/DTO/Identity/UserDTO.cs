405f325a2a1a75036aac8facef0d18af4cf7db12
Алина Лыкова <alilykka@gmail.com>
2020-06-30 20:19:26 +0300

    dTo-модели для User и Claim

---
+++ Common/WebStore.Domain/DTO/Identity/UserDTO.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.Domain.DTO.Identity
+{
+ 
+    public abstract class UserDTO
+    {
+        //пользователь, всегда будет сохранён
+        public User User { get; set; }
+    }
+
+   
+    public class AddLoginDTO : UserDTO
+    {
+        //хранит факт входа пользоателя в системы
+        public UserLoginInfo UserLoginInfo { get; set; }
+    }
+
+    
+    public class PasswordHashDTO : UserDTO
+    {
+        //возможность читать и изменять пароль. Передаём только хэш пароля
+        public string Hash { get; set; }
+    }
+     //передача инфо о времени блокировки
+     public class SetLockoutDTO : UserDTO
+    {
+        //инфо об окончании блокировки. если пусто, то не заблокирован
+        public DateTimeOffset? LockoutEnd { get; set; }
+    }
+}
