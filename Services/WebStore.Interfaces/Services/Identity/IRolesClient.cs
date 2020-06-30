8708a418a1cc06913cb137dcb4058e9c92175ccb
Алина Лыкова <alilykka@gmail.com>
2020-06-30 19:58:50 +0300

    Интерфейс ролей

---
+++ Services/WebStore.Interfaces/Services/Identity/IRolesClient.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.Interfaces.Services.Identity
+{
+    public interface IRolesClient
+        :IRoleStore<Role>
+    {
+    }
+}
