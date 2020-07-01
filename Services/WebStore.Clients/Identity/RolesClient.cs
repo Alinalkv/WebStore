5d8930cd5ddf9a969dea4689b0326c8b9e82cd6a
Алина Лыкова <alilykka@gmail.com>
2020-07-01 11:27:45 +0300

    Клиент Пользователей

---
+++ Services/WebStore.Clients/Identity/RolesClient.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Clients.Base;
+using WebStore.Domain;
+using WebStore.Interfaces.Services.Identity;
+
+namespace WebStore.Clients.Identity
+{
+    public class RolesClient : BaseClient, IRolesClient
+    {
+        public RolesClient(IConfiguration Configuration) : base(Configuration, WebApi.Identity.Roles) { }
+        
+        
+    }
+}
