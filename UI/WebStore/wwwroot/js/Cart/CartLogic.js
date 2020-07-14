eefdfef6084ae96aa5eba2940c465529fd9eca28
Алина Лыкова <alilykka@gmail.com>
2020-07-14 22:07:38 +0300

    Сделан инкремент

--- UI/WebStore/wwwroot/js/Cart/CartLogic.js
+++ UI/WebStore/wwwroot/js/Cart/CartLogic.js
@@ -15,10 +15,13 @@
     initEvents: function () {
         //находим кнопки, отвечающие за добавление товара в корзину
         $(".add-to-cart").click(Cart.addToCart);
+        $(".cart_quantity_up").click(Cart.incrementItem);
+       // $(".cart_quantity_down").click(Cart.decrementItem);
+      //  $(".cart_quantity_delete").click(Cart.removeItem);
     },
 
     //при щелчке мышки на добавление товара попадаем сюда
-    addToCart: function (event) {
+    addToCart: function(event) {
         event.preventDefault();
 
         var button = $(this);
@@ -32,7 +35,7 @@
             .fail(function () { console.log("addToCart fail"); });
     },
 
-    showToolTip: function (button) {
+    showToolTip: function(button) {
         button.tooltip({ title: "Добавлено в корзину" }).tooltip("show");
         setTimeout(function () {
             button.tooltip("destroy");
@@ -40,12 +43,70 @@
             500);
     },
 
-    refreshCartView: function () {
+    refreshCartView: function() {
         var container = $("#cart-container");
         $.get(Cart._properties.getCartViewLink)
             .done(function (cartHtml) {
                 container.html(cartHtml);
             })
             .fail(function () { console.log("getCartViewLink fail"); });
+    },
+
+    incrementItem: function(event) {
+        event.preventDefault();
+
+        var button = $(this);
+        const id = button.data("id"); //извлекаем id из словаря
+
+        var container = button.closest("tr");
+        $.get(Cart._properties.addToCartLink + "/" + id)
+            .done(function () {
+                const count = parseInt($(".cart_quantity_input", container).val());
+                $(".cart_quantity_input", container).val(count + 1);
+                Cart.refreshPrice(container);
+                Cart.refreshCartView();
+            })
+            .fail(function () { console.log("incrementItem fail"); });
+
+    },
+
+    decrementItem: function(event) {
+        event.preventDefault();
+
+        var button = $(this);
+        const id = button.data("id"); //извлекаем id из словаря
+
+    },
+
+    removeItem: function (event) {
+        event.preventDefault();
+
+        var button = $(this);
+        const id = button.data("id"); //извлекаем id из словаря
+
+    },
+
+    refreshPrice: function(container) {
+        const count = parseInt($(".cart_quantity_input", container).val());
+        const price = parseFloat($(".cart_price", container).data("price"));
+
+        const totalPrice = price * count;
+        const value = totalPrice.toLocaleString("ru-RU", { style = "currency", currency: "RUB" });
+        const cartTotalPrice = $(".cart_total_price", container);
+        cartTotalPrice.data("price", cartTotalPrice);
+        cartTotalPrice(html);
+
+        Cart.refreshTotalPrice();
+    },
+
+    refreshTotalPrice: function () {
+        var totalPrice = 0;
+        $(".cart_total_price").each(function () {
+            const price = parseFloat($(this).data("price"));
+            totalPrice += price;
+        });
+
+        const value = totalPrice.toLocaleString("ru-RU", { style = "currency", currency: "RUB" });
+        $("#total-order-price").html(value);
     }
 }
